ae8a8c53b67d9363bdf75951b30db96af81d2093
hassan <sansaliuhassan@gmail.com>
2023-08-22 11:41:22 +0100

    by hassan

---
+++ android/app/src/main/kotlin/com/example/anywheretest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sample.wireviewer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/api.dart
@@ -0,0 +1,5 @@
+class Api {
+  static String get baseUrl {
+    return "https://api.duckduckgo.com/?q=the+wire+characters&format=json";
+  }
+}
